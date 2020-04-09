ab9479e830db2057b789ee1b2f6dd85dc68b2698
Claire Couppié <claire.couppie@gmail.com>
2020-04-09 10:57:35 +0200

    MessageInput:Add a real form

--- src/components/MessageInput.js
+++ src/components/MessageInput.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const MessageInput = () => {
-  return <div />
+  const [value, setValue] = useState('');
+
+  const handleSubmit = (event)=>{
+    event.preventDefault();
+    console.log(value);
+  }
+
+  return (
+      <form onSubmit={handleSubmit}>
+        <TextField label="Type a message" onChange={e => setValue(e.target.value)} />
+        <Button variant="outlined" type="submit">SEND</Button>
+      </form>
+  )
 }
 
 export default MessageInput;
